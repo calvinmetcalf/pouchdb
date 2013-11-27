c6a163a4a563ef1ee744fdf9dddf1955b26249eb
Calvin Metcalf <cmetcalf@appgeo.com>
2013-11-26 21:30:04 -0500

    [#1043] bower, component, and build script

---
+++ bin/publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#you may need to run npm install -g tin
+tin -v $1
+git checkout -b build
+npm run build
+git add dist
+git add lib/version.js
+git add package.json
+git add bower.json
+git add component.json
+git commit -m build
+git tag $1
+git push --tags git@github.com:daleharvey/pouchdb.git $1
+npm publish
+jam publish
+git checkout master
+git branch -D build
