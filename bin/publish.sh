7fea6d8393935ec41819364b477487e1dc71caf5
Calvin Metcalf <cmetcalf@appgeo.com>
2013-11-29 13:10:32 -0500

    build

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #you may need to run npm install -g tin
-tin -v $1
+./node_modules/tin/bin/tin -v $1
+echo "module.exports = '"$1"';" > src/version.js
 git checkout -b build
 npm run build
 git add dist
@@ -10,8 +11,8 @@ git add bower.json
 git add component.json
 git commit -m build
 git tag $1
-git push --tags git@github.com:daleharvey/pouchdb.git $1
-npm publish
-jam publish
+git push --tags git@github.com:calvinmetcalf/pouchdb.git $1
+#npm publish
+#jam publish
 git checkout master
 git branch -D build
